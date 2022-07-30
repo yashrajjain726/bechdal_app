d55a044e235e84e167683956e3dea320235c9a44
Yashraj Jain <yashrajjain726@gmail.com>
2022-07-30 21:25:33 +0530

    minor changes added

---
+++ lib/screens/post/my_post_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyPostScreen extends StatelessWidget {
+  const MyPostScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(child: Text('My Post'),),
+    );
+  }
+}
